8572922d6b77f052efff32f2248f03e40f3c704a
Leomar Linhares <leomar_linhares@hotmail.com>
2022-03-15 09:57:55 -0300

    :recycle: refac: makes the Elf class have the method that shows the number of created instances of itself instead of Races class

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,20 +1,27 @@
 import Race from './Race';
 
 class Elf extends Race {
-  static createdRacesInstances = 0;
-  private _maxLifePoints: number;
+  private static _createdElfInstances = 0;
 
   constructor(
     name: string,
     dexterity: number,
+    private _maxLifePoints = 99,
   ) {
     super(name, dexterity);
-    this._maxLifePoints = 99;
-    Elf.createdRacesInstances += 1;
+    Elf.addElfInstance();
   }
 
   get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  private static addElfInstance() {
+    this._createdElfInstances += 1;
+  }
+
+  static override createdRacesInstances(): number {
+    return this._createdElfInstances;
   }
 }
 
