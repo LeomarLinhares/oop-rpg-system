e2995093f8fedc1363716b59f20d2a8d2c154a5a
Leomar Linhares <leomar_linhares@hotmail.com>
2022-03-19 12:47:20 -0300

    :construction: feat: implements the levelUp method

--- src/Character.ts
+++ src/Character.ts
@@ -25,7 +25,7 @@ abstract class Character implements Fighter {
     this._dexterity = this.race.dexterity;
     this._energy = {
       type_: this.archetype.energyType,
-      amount: getRandomInt(1, 10)
+      amount: getRandomInt(1, 10),
     };
   }
 
@@ -40,7 +40,17 @@ abstract class Character implements Fighter {
   }
 
   levelUp(): void {
-    
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+
+    this._lifePoints = this._maxLifePoints;
   }
 
   special(enemy: Fighter): void {
