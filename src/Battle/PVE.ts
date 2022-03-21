1b486b3155bce80898e0d4dde326e179c5fe192a
Leomar Linhares <leomar_linhares@hotmail.com>
2022-03-21 12:11:29 -0300

    :sparkles: feat: creates the PVE class

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,21 @@
+import Battle from './Battle';
+import Fighter, { SimpleFighter } from '../Fighter';
+
+class PVE extends Battle {
+  constructor(
+    public player: Fighter,
+    public environment: SimpleFighter[],
+  ) {
+    super(player);
+  }
+
+  override fight(): number {
+    this.environment.forEach((monster) => {
+      monster.receiveDamage(this.player.strength);
+      this.player.receiveDamage(monster.strength);
+    })
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVE;
